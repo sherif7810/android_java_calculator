43aec68aaae06c84e48d991bf9d983dd952e8154
Sherif Ali Ahmad Mohamad <she7631@hotmail.com>
2021-06-24 12:19:12 +0200

    Changes
    - Add `void buttonClicked(View)`.
    - Change layout.

--- app/src/main/java/com/example/javacalculator/MainActivity.java
+++ app/src/main/java/com/example/javacalculator/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.javacalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText mEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditText = findViewById(R.id.editText);
+    }
+
+
+    public void buttonClicked(View view) {
+        mEditText.setText(mEditText.getText().append(((Button) view).getText()));
     }
 }
